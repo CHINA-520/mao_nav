6919c1eafdf754cda7099eeb2e4d0341eba9e21a
CHINA-520 <meiyue365@outlook.com>
2025-08-07 22:53:06 +0800

    chore: 更新导航数据 - 2025/8/7 22:53:06

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -47,34 +47,6 @@ export const mockData = {
           "url": "https://x.com/home",
           "description": "X·Twitter社交媒体平台",
           "icon": "/sitelogo/x.com.ico"
-        },
-        {
-          "id": "site-1752857783656",
-          "name": "OpenAI",
-          "url": "https://chatgpt.com",
-          "description": "OpenAI,好用👌",
-          "icon": "/sitelogo/chatgpt.com.ico"
-        },
-        {
-          "id": "linux-do",
-          "name": "Linux.do",
-          "url": "https://linux.do",
-          "description": "Linux技术社区，Peace and Love",
-          "icon": "/sitelogo/linux.do.ico"
-        },
-        {
-          "id": "site-1752649007053",
-          "name": "BeJson",
-          "url": "https://www.bejson.com/",
-          "description": "工具大全",
-          "icon": "/sitelogo/www.bejson.com.ico"
-        },
-        {
-          "id": "curlconverter",
-          "name": "curl converter",
-          "url": "https://curlconverter.com/",
-          "description": "curl命令转换工具",
-          "icon": "/sitelogo/curlconverter.com.ico"
         }
       ]
     },
